a446db48ac89a566170cbe14b39d5a6b8cabe540
noemie graignic <noemie.graignic@free.fr>
2019-11-08 20:51:11 +0100

    Intégration de Firebase à mon application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'bookshelves';
+
+  constructor(){
+    const firebaseConfig = {
+      apiKey: "AIzaSyD3aJurh9bCCxFwfvp_BKCwJts-OrEprpg",
+      authDomain: "bookshelves-dcfa7.firebaseapp.com",
+      databaseURL: "https://bookshelves-dcfa7.firebaseio.com",
+      projectId: "bookshelves-dcfa7",
+      storageBucket: "bookshelves-dcfa7.appspot.com",
+      messagingSenderId: "18646196046",
+      appId: "1:18646196046:web:a810e535f16fbef1a3481f",
+      measurementId: "G-DPV6K2K1EQ"
+    };
+  }
 }
